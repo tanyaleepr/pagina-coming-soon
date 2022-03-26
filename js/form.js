42c16f232a064e39d225eb2423fd9668e777e874
tanyaleepr <tanyaleepr@gmail.com>
2022-03-26 17:41:31 -0500

    developed

--- js/form.js
+++
@@ -1,8 +0,0 @@
-function mailMe(form){
-    Subject=document.testform.inputbox1.value
-    CC= document.testform.inputbox2.value
-    BCC= document.testform.inputbox3.value
-    location = "/javaworld/cgi-bin/jw-mailto.cgi?tanyaleepr@gmail.com?subject="+Subject+"&Bcc="+
-        BCC+"&cc="+CC
-    document.testform.submit();
-}
--- js/scripts.js
+++ js/scripts.js
@@ -3,119 +3,48 @@
 
 (function($) {
     "use strict"; 
-    
-    /* Particles */
-	particlesJS("particles-js", {
-		"particles": {
-		"number": {
-			"value": 120,
-			"density": {
-			"enable": true,
-			"value_area": 1200
-			}
-		},
-		"color": {
-			"value": "#585f63"
-		},
-		"shape": {
-			"type": "circle",
-			"stroke": {
-			"width": 0,
-			"color": "#000000"
-			},
-			"polygon": {
-			"nb_sides": 5
-			},
-			"image": {
-			"src": "img/github.svg",
-			"width": 100,
-			"height": 100
-			}
-		},
-		"opacity": {
-			"value": 0.2,
-			"random": false,
-			"anim": {
-			"enable": false,
-			"speed": 1,
-			"opacity_min": 0.2,
-			"sync": false
-			}
-		},
-		"size": {
-			"value": 3,
-			"random": true,
-			"anim": {
-			"enable": false,
-			"speed": 40,
-			"size_min": 0.1,
-			"sync": false
-			}
-		},
-		"line_linked": {
-			"enable": true,
-			"distance": 150,
-			"color": "#585f63",
-			"opacity": 0.4,
-			"width": 1
-		},
-		"move": {
-			"enable": true,
-			"speed": 6,
-			"direction": "none",
-			"random": false,
-			"straight": false,
-			"out_mode": "out",
-			"bounce": false,
-			"attract": {
-			"enable": false,
-			"rotateX": 600,
-			"rotateY": 1200
-			}
+	
+    /* Navbar Scripts */
+    // jQuery to collapse the navbar on scroll
+    $(window).on('scroll load', function() {
+		if ($(".navbar").offset().top > 60) {
+			$(".fixed-top").addClass("top-nav-collapse");
+		} else {
+			$(".fixed-top").removeClass("top-nav-collapse");
 		}
-		},
-		"interactivity": {
-		"detect_on": "canvas",
-		"events": {
-			"onhover": {
-			"enable": true,
-			"mode": "grab"
-			},
-			"onclick": {
-			"enable": true,
-			"mode": "push"
-			},
-			"resize": true
-		},
-		"modes": {
-			"grab": {
-			"distance": 140,
-			"line_linked": {
-				"opacity": 1
-			}
-			},
-			"bubble": {
-			"distance": 400,
-			"size": 40,
-			"duration": 2,
-			"opacity": 8,
-			"speed": 3
-			},
-			"repulse": {
-			"distance": 200,
-			"duration": 0.4
-			},
-			"push": {
-			"particles_nb": 4
-			},
-			"remove": {
-			"particles_nb": 2
-			}
-		}
-		},
-		"retina_detect": true
     });
     
+	// jQuery for page scrolling feature - requires jQuery Easing plugin
+	$(function() {
+		$(document).on('click', 'a.page-scroll', function(event) {
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
+			}, 600, 'easeInOutExpo');
+			event.preventDefault();
+		});
+    });
+
+    // offcanvas script from Bootstrap + added element to close menu on click in small viewport
+    $('[data-toggle="offcanvas"], .navbar-nav li a:not(.dropdown-toggle').on('click', function () {
+        $('.offcanvas-collapse').toggleClass('open')
+    })
+
+    // hover in desktop mode
+    function toggleDropdown (e) {
+        const _d = $(e.target).closest('.dropdown'),
+            _m = $('.dropdown-menu', _d);
+        setTimeout(function(){
+            const shouldOpen = e.type !== 'click' && _d.is(':hover');
+            _m.toggleClass('show', shouldOpen);
+            _d.toggleClass('show', shouldOpen);
+            $('[data-toggle="dropdown"]', _d).attr('aria-expanded', shouldOpen);
+        }, e.type === 'mouseleave' ? 300 : 0);
+    }
+    $('body')
+    .on('mouseenter mouseleave','.dropdown',toggleDropdown)
+    .on('click', '.dropdown-menu a', toggleDropdown);
+
 
     /* Move Form Fields Label When User Types */
     // for input and textarea fields
@@ -125,6 +54,19 @@
 		} else {
 			$(this).removeClass('notEmpty');
 		}
+	});
+	
+
+    /* Back To Top Button */
+    // create the back to top button
+    $('body').prepend('<a href="body" class="back-to-top page-scroll">Back to Top</a>');
+    var amountScrolled = 700;
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > amountScrolled) {
+            $('a.back-to-top').fadeIn('500');
+        } else {
+            $('a.back-to-top').fadeOut('500');
+        }
     });
 
 
